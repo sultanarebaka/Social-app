70020d1d467087fc5c6308594ae04097c9348bd9
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-07-03 14:53:10 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 let form = document.getElementById("form");
 let input = document.getElementById("input");
 let message = document.getElementById("message");
-let posts = document.getElementById("posts")
+let posts = document.getElementById("posts");
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
+    console.log("button clicked");
+    formValidation();
+  });
 
