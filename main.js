4ad5fe116cae2fbe7ee07a8f69b88a42d0388cad
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-07-03 14:39:48 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let message = document.getElementById("message");
+let posts = document.getElementById("posts")
+
+
+
